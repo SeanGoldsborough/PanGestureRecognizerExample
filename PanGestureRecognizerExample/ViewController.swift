a281a17131e0d1997ddbf4a3d903b8058e11a406
Sean Goldsborough <smgoldsborough@gmail.com>
2018-08-28 12:36:37 -0400

    feat: adds pan gesture func

--- PanGestureRecognizerExample/ViewController.swift
+++ PanGestureRecognizerExample/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var fileViewOrigin: CGPoint!
 
+    @IBOutlet weak var fileImageView: UIImageView!
+    @IBOutlet weak var trashImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addPanGesture(view: fileImageView)
+        fileViewOrigin = fileImageView.frame.origin
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addPanGesture(view: UIView){
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
     }
-
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let fileView = sender.view!
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            // This enables us to move the file view along with the gesture recognizer
+            fileImageView.center = CGPoint(x: fileView.center.x + translation.x, y: fileView.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in: view)
+        case .ended:
+            break
+        default:
+            break
+        }
+    }
 
 }
 
