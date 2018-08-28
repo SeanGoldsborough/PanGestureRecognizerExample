822d100737571e8d74d0e97b913ae4e3371aeff8
Sean Goldsborough <smgoldsborough@gmail.com>
2018-08-28 16:20:00 -0400

    feat: creates new file image programmatically after an file is thrown away

--- PanGestureRecognizerExample/ViewController.swift
+++ PanGestureRecognizerExample/ViewController.swift
@@ -14,15 +14,31 @@ class ViewController: UIViewController {
     var fileViewOrigin: CGPoint!
     var audioPlayer = AVAudioPlayer()
 
-    @IBOutlet weak var fileImageView: UIImageView!
+    //@IBOutlet weak var fileImageView: UIImageView!
+    weak var newImageView: UIImageView!
     @IBOutlet weak var trashImageView: UIImageView!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        addPanGesture(view: fileImageView)
-        fileViewOrigin = fileImageView.frame.origin
-        view.bringSubview(toFront: fileImageView)
         createAudioPlayer()
+        createImageView()
+    }
+    
+    func createImageView() {
+        let imageName = "file.png"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        newImageView = imageView
+        newImageView.alpha = 1.0
+        newImageView.isUserInteractionEnabled = true
+        print("self.fileImageView is now: \(newImageView)")
+        
+        newImageView.frame = CGRect(x: 20, y: 20, width: 128, height: 128)
+        
+        view.addSubview(newImageView)
+        addPanGesture(view: newImageView)
+        fileViewOrigin = newImageView.frame.origin
+        view.bringSubview(toFront: newImageView)
     }
     
     func createAudioPlayer() {
@@ -62,21 +78,22 @@ class ViewController: UIViewController {
     func moveViewWithPan(fileView: UIView, sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: view)
         // This enables us to move the file view along with the gesture recognizer
-        fileImageView.center = CGPoint(x: fileView.center.x + translation.x, y: fileView.center.y + translation.y)
+        newImageView.center = CGPoint(x: fileView.center.x + translation.x, y: fileView.center.y + translation.y)
         sender.setTranslation(CGPoint.zero, in: view)
     }
     
     func returnViewToOrigin(view:UIView) {
         // This moves the file image view back to the point of origin if
         // it does not intersect with the trash image view
-        UIView.animate(withDuration: 0.3, animations: {self.fileImageView.frame.origin = self.fileViewOrigin})
+        UIView.animate(withDuration: 0.3, animations: {self.newImageView.frame.origin = self.fileViewOrigin})
     }
     
     func deleteView(view: UIView) {
         // This fades the file image view out when it intersects with the trash image view
+        UIView.animate(withDuration: 0.3, animations: {self.newImageView.alpha = 0.0})
         audioPlayer.play()
-        UIView.animate(withDuration: 0.3, animations: {self.fileImageView.alpha = 0.0})
-        self.fileImageView = nil
-        print("self.fileImageView is:\(self.fileImageView)")
+        self.newImageView = nil
+        print("self.fileImageView is:\(self.newImageView)")
+        createImageView()
     }
 }
